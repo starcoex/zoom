14fa25c257877304465a553be960bf09a1202a9e
starcoex <69073390+starcoex@users.noreply.github.com>
2022-10-31 14:54:31 +0900

    first Websoket setting

--- src/server.js
+++ src/server.js
@@ -1,4 +1,6 @@
+import http from "http";
 import express from "express";
+import WebSocket from "ws";
 
 const app = express();
 const PORT = 4100;
@@ -7,7 +9,14 @@ app.use("/public", express.static(process.cwd() + "/src/public"));
 app.set("views", process.cwd() + "/src/views");
 
 app.get("/", (req, res) => res.render("home"));
+app.get("/*", (req, res) => res.redirect("/"));
 
-app.listen(PORT, () => {
-  console.log(`Start Zoom http://localhost:${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Start Zoom http://localhost:${PORT}`);
+// });
+
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+server.listen(PORT, (req, res) =>
+  console.log(`Zoom WebSocket Sever Start http://localhost:${PORT} `)
+);
