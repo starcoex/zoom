23930c394005d9ea097e6800cd6c7cdc728bd26d
starcoex <69073390+starcoex@users.noreply.github.com>
2022-10-31 11:25:32 +0900

    first, frondend setup setting

---
+++ src/public/js/app.js
@@ -0,0 +1,1 @@
+console.log("<Hello322323232>sss</Hello322323232>asdfasd");
--- src/server.js
+++ src/server.js
@@ -2,6 +2,11 @@ import express from "express";
 
 const app = express();
 const PORT = 4100;
+app.set("view engine", "pug");
+app.use("/public", express.static(process.cwd() + "/src/public"));
+app.set("views", process.cwd() + "/src/views");
+
+app.get("/", (req, res) => res.render("home"));
 
 app.listen(PORT, () => {
   console.log(`Start Zoom http://localhost:${PORT}`);
