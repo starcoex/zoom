ff7a8fd0744e41cb505aa5095ee8ec3f55d7e238
starcoex <69073390+starcoex@users.noreply.github.com>
2022-11-04 12:01:50 +0900

    first socketio setting

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,39 +1,41 @@
-const messageList = document.querySelector("ul");
-const messageForm = document.querySelector("#message");
-const nickForm = document.querySelector("#nick");
+const socket = io();
 
-const socket = new WebSocket(`ws://${window.location.host}`);
+// const messageList = document.querySelector("ul");
+// const messageForm = document.querySelector("#message");
+// const nickForm = document.querySelector("#nick");
 
-const makeMessage = (type, payload) => {
-  const msg = { type, payload };
-  return JSON.stringify(msg);
-};
+// const socket = new WebSocket(`ws://${window.location.host}`);
 
-socket.addEventListener("open", () => console.log("Connection Server"));
-socket.addEventListener("message", (message) => {
-  const li = document.createElement("li");
-  messageList.append(li);
-  li.innerText = message.data;
-});
+// const makeMessage = (type, payload) => {
+//   const msg = { type, payload };
+//   return JSON.stringify(msg);
+// };
 
-const handleMessageSubmit = (event) => {
-  event.preventDefault();
-  const input = messageForm.querySelector("input");
-  // let inputValue = input.value;
-  socket.send(makeMessage("New_Message", input.value));
-  input.value = "";
-};
+// socket.addEventListener("open", () => console.log("Connection Server"));
+// socket.addEventListener("message", (message) => {
+//   const li = document.createElement("li");
+//   messageList.append(li);
+//   li.innerText = message.data;
+// });
 
-const handleNickSubmit = (event) => {
-  event.preventDefault();
-  const input = nickForm.querySelector("input");
-  socket.send(makeMessage("nickName", input.value));
-  input.value = "";
-};
+// const handleMessageSubmit = (event) => {
+//   event.preventDefault();
+//   const input = messageForm.querySelector("input");
+//   // let inputValue = input.value;
+//   socket.send(makeMessage("New_Message", input.value));
+//   input.value = "";
+// };
 
-messageForm.addEventListener("submit", handleMessageSubmit);
-nickForm.addEventListener("submit", handleNickSubmit);
+// const handleNickSubmit = (event) => {
+//   event.preventDefault();
+//   const input = nickForm.querySelector("input");
+//   socket.send(makeMessage("nickName", input.value));
+//   input.value = "";
+// };
 
-socket.addEventListener("close", () =>
-  console.log("DisConnection from Server")
-);
+// messageForm.addEventListener("submit", handleMessageSubmit);
+// nickForm.addEventListener("submit", handleNickSubmit);
+
+// socket.addEventListener("close", () =>
+//   console.log("DisConnection from Server")
+// );
--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 import http from "http";
+import SocketIO from "socket.io";
 import express from "express";
-import WebSocket from "ws";
 
 const app = express();
 const PORT = 4100;
@@ -11,37 +11,34 @@ app.set("views", process.cwd() + "/src/views");
 app.get("/", (req, res) => res.render("home"));
 app.get("/*", (req, res) => res.redirect("/"));
 
-const sockets = [];
 const server = http.createServer(app);
-const wss = new WebSocket.Server({ server });
-wss.on("connection", (socket) => {
-  sockets.push(socket);
-  socket["nickName"] = "Anon";
-  console.log(sockets);
-  console.log("Connected to Brower");
-  socket.on("close", () => console.log("Disconnected to Client"));
-  socket.on("message", (message) => {
-    const parsed = JSON.parse(message);
-    switch (parsed.type) {
-      case "New_Message":
-        sockets.forEach((aSocket) => {
-          aSocket.send(`${socket.nickName}: ${parsed.payload.toString()}`);
-        });
-      case "nickName":
-        socket["nickName"] = parsed.payload.toString();
-        break;
-    }
-
-    // if (parsed.type === "New_Message") {
-    //   sockets.forEach((aSocket) => {
-    //     aSocket.send(`${socket.nickName}: ${parsed.payload.toString()}`);
-    //   });
-    // } else if (parsed.type === "nickName") {
-    //   socket["nickName"] = parsed.payload.toString();
-    // }
-  });
-  // socket.on("message", (message) => socket.send(message.toString()));
+const io = SocketIO(server);
+io.on("connection", (socket) => {
+  console.log(socket);
 });
+
+// const sockets = [];
+// const server = http.createServer(app);
+// const wss = new WebSocket.Server({ server });
+// wss.on("connection", (socket) => {
+//   sockets.push(socket);
+//   socket["nickName"] = "Anon";
+//   console.log(sockets);
+//   console.log("Connected to Brower");
+//   socket.on("close", () => console.log("Disconnected to Client"));
+//   socket.on("message", (message) => {
+//     const parsed = JSON.parse(message);
+//     switch (parsed.type) {
+//       case "New_Message":
+//         sockets.forEach((aSocket) => {
+//           aSocket.send(`${socket.nickName}: ${parsed.payload.toString()}`);
+//         });
+//       case "nickName":
+//         socket["nickName"] = parsed.payload.toString();
+//         break;
+//     }
+//   });
+// });
 
 server.listen(PORT, (req, res) =>
   console.log(`Zoom WebSocket Sever Start http://localhost:${PORT} `)
